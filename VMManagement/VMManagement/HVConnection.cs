b11127ba4f7533ec44691849e31581f1c4cd9d6a
Tomislav Dukarić <tomislav_dukaric@hotmail.com>
2015-11-04 07:31:15 +0100

    ExampleAdd Addition

---
+++ VMManagement/VMManagement/HVConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMManagement
+{
+    public static class HVConnection
+    {
+        public static ManagementScope GetHVScope(string hostName)
+        {
+            return new ManagementScope(@"\\" + hostName + "\root\virtualization");
+        }
+        
+        
+    }
+}
