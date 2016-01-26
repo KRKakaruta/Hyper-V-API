a87648ac7c29ee86d49d0e4ba0226e88796bc442
Tomislav Dukarić <tomislav@dukaric.net>
2016-01-26 21:01:03 +0100

    Added supported classes

---
+++ VMManagement/SupportClassesPortable/Server.cs
@@ -0,0 +1,68 @@
+﻿using SupportClassesPortable.Networking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace SupportClassesPortable
+{
+    public enum ServerType
+    {
+        HyperVv1,
+        HyperVv2,
+        ESXi
+    }
+    public interface IServer
+    {
+        void SetHostName(string name);
+        string GetHostName();
+        void SetIPv4Address(string address, string gateway, int netMask);
+        void SetIPV4Address(NetworkAddressIPv4 ip);
+        void SetHypervisorType(ServerType type);
+    }
+
+    public abstract class Server : IServer
+    {
+        string hostName;
+        NetworkAddressIPv4 IPv4Address;
+
+        ServerType hypervisorType;
+
+        public void SetHostName(string name)
+        {
+            this.hostName = name;
+        }
+
+        public string GetHostName()
+        {
+            return this.hostName;
+        }
+
+        public void SetIPv4Address(string address, string gateway = null, int netMask = 0)
+        {
+            if (this.IPv4Address == null)
+                this.IPv4Address = new NetworkAddressIPv4();
+
+            this.IPv4Address.SetAddress(address);
+
+            if (gateway != null)
+                this.IPv4Address.SetGateway(gateway);
+
+            if (netMask != 0)
+                this.IPv4Address.SetNetMask(netMask);
+
+        }        
+
+        public void SetIPV4Address(NetworkAddressIPv4 ip)
+        {
+            this.IPv4Address = ip;
+        }
+
+        public void SetHypervisorType(ServerType type)
+        {
+            this.hypervisorType = type;
+        }
+    }
+}
---
+++ VMManagement/SupportClassesPortable/VirtualMachine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportClassesPortable
+{
+    public class VirtualMachine
+    {
+        string name;
+        IServer Server;
+
+        public VirtualMachine(string name, IServer server)
+        {
+            this.name = name;
+            this.Server = server;
+        }
+    }
+}
